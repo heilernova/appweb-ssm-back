63c5a5f08625e69f595583254899982db85970db
HeilerNova <81369584+HeilerNova@users.noreply.github.com>
2022-07-31 00:34:40 -0500

    Correccion

--- src/Controllers/sac/cases/files/files-get.php
+++ src/Controllers/sac/cases/files/files-get.php
@@ -4,19 +4,13 @@ use HNova\Rest\req;
 use HNova\Rest\res;
 
 $case_id = req::params()->caseId;
-$name_file = str_replace('%20', ' ', req::params()->name );
-// // $name_file = urldecode(req::params()->name);
-// return res::text($name_file)->status(500);
+$name_file = req::params()->name;
 
-// return res::file(__DIR__ . '/cases-post.php');
-
-$dir = $_ENV['api-rest-dir'] . '/../files/cases/' . str_pad($case_id, 5, '0', STR_PAD_LEFT);
-// return $dir;
+$dir = $_ENV['api-rest-dir'] . '/files/cases/' . str_pad($case_id, 5, '0', STR_PAD_LEFT);
 if ( file_exists($dir) ){
 
     $path = "$dir/$name_file";
 
-    // return $dir;
     if ( file_exists($path) ){
         return res::file($path);
     }
