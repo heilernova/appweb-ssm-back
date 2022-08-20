34d2f5f25fd7c4c2fcbcae74fb2a1d03d303cef5
HeilerNova <81369584+HeilerNova@users.noreply.github.com>
2022-08-19 19:11:14 -0500

    Actualizacion

--- src/Controllers/Surveys/questions/question.php
+++ src/Controllers/Surveys/questions/question.php
@@ -6,7 +6,7 @@ class question{
     public const TYPE_RADIO_BUTTON = 2;
     public const TYPE_RADIO_BUTTON_MAS = 3;
 
-    public const OPTIONS_YES_NO = [ [ 'code' => 'Si', 'text' => 'Si' ], [ 'code' => 'Si', 'text' => 'Si' ] ];
+    public const OPTIONS_YES_NO = [ [ 'code' => 'Si', 'text' => 'Si' ], [ 'code' => 'No', 'text' => 'No' ] ];
     public const OPTIONS_OKEY_REGULAR_BAB = [ ['code' => 'Bueno', 'text' => 'Bueno'], ['code' => 'Regular', 'text' => 'Regular'], ['code' => 'Malo', 'text' => 'Malo'] ];
     public const OPTIONS_SCORE_1_TO_5 = [ ['code' => 1, 'text' => '1'], ['code' => 2, 'text' => '2'], ['code' => 3, 'text' => '3'], ['code' => 4, 'text' => '4'], ['code' => 5, 'text' => '5']];
 
