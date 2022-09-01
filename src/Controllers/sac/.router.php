b97e168cd80cd110a2a4f559bdd1d89f1dc64bf4
Heile Nova <heilernova@outlook.com>
2022-09-01 12:24:16 -0500

    Actualizacion

--- src/Controllers/sac/.router.php
+++ src/Controllers/sac/.router.php
@@ -1,12 +1,19 @@
 <?php
 
+use HNova\Db\db;
+use HNova\Db\Pull;
 use HNova\Rest\req;
 use HNova\Rest\router;
 
 
 router::use('/cases', fn() => require __DIR__.'/cases/.router.php');
-// router::get('/cases', fn() => require __DIR__ . '/cases-get-all.php');
-
-// router::post('/cases/:id/comments', fn() => require __DIR__ . '/comment-post.php');
 
 router::get('/data', fn() => require __DIR__ . '/get-data.php');
+
+router::put('/comments/:id', function(){
+    $id = req::params()->id;
+    $content = req::body();
+    $db = new Pull();
+    $sql = "UPDATE tb_sac_cases_comments SET content = ? where id = ?";
+    return $db->query($sql, [$content, $id])->rowCount > 0;
+});
